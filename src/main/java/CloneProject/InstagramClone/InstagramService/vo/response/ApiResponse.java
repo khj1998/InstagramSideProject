01eab6da9e5c08985c03eb25f83b06b178487084
kimhojin98@naver.com <kimhojin98@naver.com>
2023-03-02 22:47:05 +0900

    ENH: API응답 스펙 통일

---
+++ src/main/java/CloneProject/InstagramClone/InstagramService/vo/response/ApiResponse.java
@@ -0,0 +1,55 @@
+package CloneProject.InstagramClone.InstagramService.vo.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.http.ResponseEntity;
+
+@JsonPropertyOrder({"success","message","data"})
+public class ApiResponse<T> {
+    public boolean success;
+    public String message;
+    public T data;
+
+    private ApiResponse(ApiResponseBuilder<T> builder) {
+        this.success = builder.success;
+        this.message = builder.message;
+        this.data = builder.data;
+    }
+
+    public boolean getSuccess() {
+        return this.success;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public static class ApiResponseBuilder<T> {
+        private boolean success;
+        private String message;
+        private T data;
+
+        public ApiResponseBuilder<T> success(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public ApiResponseBuilder<T> message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public ApiResponseBuilder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public ResponseEntity<ApiResponse> build() {
+            ApiResponse apiResponse = new ApiResponse(this);
+            return ResponseEntity.ok(apiResponse);
+        }
+    }
+}
