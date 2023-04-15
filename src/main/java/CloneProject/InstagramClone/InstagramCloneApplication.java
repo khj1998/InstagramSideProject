264d3b3014bab739dcf63af573e6c2ef6d2d4965
kimhojin98@naver.com <kimhojin98@naver.com>
2023-04-15 23:19:28 +0900

    ENH:쓸모없는 주입 제거

--- src/main/java/CloneProject/InstagramClone/InstagramCloneApplication.java
+++ src/main/java/CloneProject/InstagramClone/InstagramCloneApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public class InstagramCloneApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(InstagramCloneApplication.class, args);
 	}
 }
